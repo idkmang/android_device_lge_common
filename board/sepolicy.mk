4850f64c2c25a4ac614ae9a08955fba65ad958d5
idkmang <idkmang@gmail.com>
2018-05-05 10:57:34 -0700

    first commit

---
+++ board/sepolicy.mk
@@ -0,0 +1,5 @@
+# SELinux
+#BOARD_SEPOLICY_DIRS += \
+#    $(VENDOR_PATH)/sepolicy
+
+#include device/qcom/sepolicy/sepolicy.mk
---
+++ product/keylayout.mk
@@ -0,0 +1,4 @@
+# Keylayouts
+PRODUCT_COPY_FILES += \
+    $(VENDOR_PATH)/keylayout/ft5x06_720p.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/ft5x06_720p.kl \
+    $(VENDOR_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
